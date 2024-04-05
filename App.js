6c432efcba3b53c527d3a4873914d28a49a24a24
tbboy1812@gmail.com <tbboy1812@gmail.com>
2024-04-05 14:45:06 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Image, Alert } from 'react-native';
+import { Platform } from 'react-native';
+const  App= ()=>
+{
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default function App() {
+  const onPress= ()=> {
+    const message = `Đăng nhập với:\nTaiKhoan: ${username}\nMatkhau: ${password}`;
+    if (Platform.OS === 'web') {
+      window.alert(message); 
+    } else {
+      Alert.alert('Thông tin đăng nhập', message); 
+    }
+  };
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('./assets/background.jpg')} style={styles.img}>
+      <View style={ styles.container}>
+        <Image source={require('./assets/hinhnen_pokemongo.jpg')} style={styles.logo} />
+       <Text style={{color:"white", marginLeft:250}}>
+         Register
+       </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={setUsername}
+          value={username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity style={styles.loginButton} onPress={onPress}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
+
 const styles = StyleSheet.create({
+  img :{
+    marginLeft:500,
+  },
   container: {
+    marginLeft : 60,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+ 
+  
+  logo: {
+    width: 300,
+    height: 150,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: 'green',
+    width: '100%',
+    padding: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+  },
+  loginButton: {
+    width: '100%',
+    backgroundColor: '#007bff',
+    padding: 10,
+    borderRadius: 4,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+
+  backgroundImageStyle: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+export default App;
